39cc279e6957468c4aa01b28b26549f71b403626
Your Name <karolfurtak@o2.pl>
2026-05-10 20:23:45 +0200

    CI: ignoruj E701/E702/E703 (stylistyczne) w ruff + shellcheck severity=error

--- app/Anbernet.sh
+++ app/Anbernet.sh
@@ -5,6 +5,6 @@ export HOME=/root
 export PATH="/root/.local/bin:/usr/local/bin:/usr/bin:/bin"
 LOG=/mnt/data/anbernet.log
 echo "$(date +%H:%M:%S): start" >> "$LOG"
-cd /mnt/mmc/Roms/APPS/anbernet
+cd /mnt/mmc/Roms/APPS/anbernet || exit 1
 python3 main.py >> "$LOG" 2>&1
 echo "$(date +%H:%M:%S): exit $?" >> "$LOG"
